dd2a14eadb8d157c51b4cba8df40587066355506
Valk <atoons@gmail.com>
2021-11-17 16:09:28 +1100

    update config to have empty strings if values are missing

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,13 @@
-export default {
-  token: process.env.BOT_TOKEN,
-  client_id: process.env.CLIENT_ID,
-  prefix: process.env.COMMAND_PREFIX
+interface Config {
+  token: string,
+  client_id: string,
+  prefix: string
 }
+
+const config: Config = {
+  token: process.env.BOT_TOKEN || '',
+  client_id: process.env.CLIENT_ID || '',
+  prefix: process.env.COMMAND_PREFIX || ''
+}
+
+export default config
